c16294ec43db40d8cdebf5c9547ec646aef97b3e
Aleksandr Sobolev <aleksandr_sobolev2@epam.com>
2025-04-09 20:17:52 +0400

    random debug

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ app.post('/upload', upload.single('image'), imageController.uploadImage);
 
 app.get('/download/:fileName', imageController.downloadImage);
 
-app.get('/metadata/:fileName', imageController.getImageMetadata);
-
 app.get('/metadata/random', imageController.getRandomImageMetadata);
+
+app.get('/metadata/:fileName', imageController.getImageMetadata);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}\n`);
